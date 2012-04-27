0e42cc6c5c7c2dee878df86e0f20fb77fa2b835f
Kimson Wong <kimson.wong@teamcodeflux.com>
2012-04-28 01:44:39 +0800

    Initial rails project
    Signed-off-by: Kimson Wong <kimson.wong@teamcodeflux.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carpool::Application.config.secret_token = '65cb9d56527af3f1ff20dfccb115d4e6076152ededd863a862825dfa0382f7899877f6cc5c3320eb3440c095e8ce1134709d42d7daa582e19cbbc609b72bc525'
