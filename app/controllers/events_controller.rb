c29580ce8bb6866fc348489554588cb3d8fda871
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-04-28 18:55:53 +0800

    Only display events of the currently logged in user
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
-
+    @jaja = "Hello"
+    @events = Event.find_all_by_user_id(current_user.id)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @events }
