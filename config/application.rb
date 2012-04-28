e4e0e51bc44f2efcdab4d0f68d29b7c1b449a70e
Kimson Wong <kimson.wong@teamcodeflux.com>
2012-04-28 15:29:05 +0800

    Changed timezone to GMT+8
    Signed-off-by: Kimson Wong <kimson.wong@teamcodeflux.com>

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Carpool
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.time_zone = 'Taipei'
   end
 end
