f667548b37c686a2f170c0865de25284c1b688a9
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-04-28 15:44:54 +0800

    Facebook login implementation
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Carpool::Application.routes.draw do
   resources :invitations
-
   resources :passengers
-
   resources :routes
-
   resources :events
-
   resources :cities
-
   resources :users
+
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/signout' => 'sessions#destroy', :as => :signout
 
   root :to => 'home#index'
 
