eb52b3cefbd152277f418bdf7bca3fb118185ce7
Kimson Wong <kimson.wong@teamcodeflux.com>
2012-04-28 15:56:33 +0800

    Added seeds for City model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  
+City.create(:name => "Caloocan")
+City.create(:name => "Las Pinas")
+City.create(:name => "Makati City")
+City.create(:name => "Malabon")
+City.create(:name => "Mandaluyong")
+City.create(:name => "Manila")
+City.create(:name => "Marikina")
+City.create(:name => "Muntinlupa")
+City.create(:name => "Navotas")
+City.create(:name => "Paranaque")
+City.create(:name => "Pasay")
+City.create(:name => "Pasig")
+City.create(:name => "Pateros")
+City.create(:name => "Quezon City")
+
+
+
+
+
+
+
+
+
+
