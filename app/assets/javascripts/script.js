e24ca826367ac1a21149274458c8841b48cc17e5
Kimson Wong <kimson.wong@teamcodeflux.com>
2012-04-28 23:16:11 +0800

    Update on css and javascript

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,17 +1,39 @@
 /* Author: Mark Conde
 
 */
+var search_mode = 0;
+function switchSearch(){
+	if(search_mode == 0){
+		search_mode = 1;
+		$("#search-filter").attr("value","1");
+		console.log($("#search-filter").attr("value"));
+		var searchclone = $("#ride-search");
+		var textclone = $("#right-section > div");
+		$("#left-section").fadeOut('fast',function(){
+			$("#left-section").append(textclone);
+			$("#btn-search_for_passengers").attr("src","img/btn-search_for_a_ride.png");
+		}).fadeIn();
+		$("#right-section").fadeOut('fast',function(){
+			$("#right-section").append(searchclone);
+		}).fadeIn();
+	}
+	else{
+		search_mode = 0;
+		$("#search-filter").attr("value","0");
+		console.log($("#search-filter").attr("value"));
+		var searchclone = $("#ride-search");
+		var textclone = $("#left-section > div");
+		$("#right-section").fadeOut('fast',function(){
+			$("#left-section").append(searchclone);
+			$("#btn-search_for_passengers").attr("src","img/btn-search_for_passengers.png");
+		}).append(textclone).fadeIn();
+		$("#left-section").fadeOut('fast',function(){
+			$("#right-section").append(textclone);
+		}).append(searchclone).fadeIn();
+	}
+}
 $("document").ready(function(){
 	$("#btn-search_for_passengers").click(function(){
-		var searchclone = $("#ride-search").clone();
-		var textclone = $("#right-section > div").clone();
-		$("#ride-search").fadeOut('slow',function(){
-			$("#ride-search").remove();
-			$("#right-section > div").remove();
-			$("#left-section").append(textclone);
-			$("#right-section").append(searchclone);
-		});
-		
-		$("#right-section > div").fadeOut('slow');
+		switchSearch();
 	});
 });
